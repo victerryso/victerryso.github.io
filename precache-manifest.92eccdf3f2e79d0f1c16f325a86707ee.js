4b99aebdbc015e488c2cdb650c38e4f6fbf261d3
victerryso <victerry.so@gmail.com>
2020-04-27 02:06:11 +1000

    Updates

---
+++ precache-manifest.92eccdf3f2e79d0f1c16f325a86707ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16955fcea1f3574eff7308d359042bb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73227179023410b55101",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4d617ab55c7ce1539057",
+    "url": "/static/js/2.c176a662.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c176a662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73227179023410b55101",
+    "url": "/static/js/main.179d0a19.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  }
+]);
